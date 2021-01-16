add711e993af557fc6de3c85373adfa453462338
taiseiyo <53172326+taiseiyo@users.noreply.github.com>
2021-01-16 17:12:31 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import firebase from "firebase/app";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const firebaseConfig = {
+  apiKey: "AIzaSyA_Bx8VfyUxO1twJQrZ9-u37fcNF3lwKrI",
+  authDomain: "word-test-916dc.firebaseapp.com",
+  projectId: "word-test-916dc",
+  storageBucket: "word-test-916dc.appspot.com",
+  messagingSenderId: "482569027636",
+  appId: "1:482569027636:web:393808c6c616ab7f0e4a61",
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "testx",
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div>{this.state.text}</div>
+        <div>fask</div>
+      </div>
+    );
+  }
 }
 
 export default App;
